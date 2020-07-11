59cec81d18c0a61a07ebc410c8dbd2152f7d748a
Joseph Jin <jerkjoe61@gmail.com>
2020-07-10 20:40:21 -0700

    lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,19 @@
 module.exports = {
-    env: {
-        browser: true,
-        es2020: true
+  env: {
+    browser: true,
+    es2020: true,
+  },
+  extends: ['plugin:react/recommended', 'airbnb', 'plugin:prettier/recommended'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    extends: ['plugin:react/recommended', 'airbnb', "plugin:prettier/recommended"],
-    parserOptions: {
-        ecmaFeatures: {
-            jsx: true
-        },
-        ecmaVersion: 11,
-        sourceType: 'module'
-    },
-    plugins: ['react'],
-    rules: {
-        'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }]
-    }
-};
+    ecmaVersion: 11,
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    'react/jsx-filename-extension': [1, {extensions: ['.js', '.jsx']}],
+    'react/jsx-one-expression-per-line': [0],
+  },
+}
